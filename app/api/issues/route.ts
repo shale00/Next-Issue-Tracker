04846dc9e4d62cd10cc6ad32f31470aa8be19b21
sth <sth0067@gmail.com>
2023-10-24 19:15:18 -0500

    built an api for creating an issue

---
+++ app/api/issues/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import {  z } from 'zod';
+import prisma from '@/prisma/client';
+
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+
+    const createIsssueSchema = await z.object({
+        title: z.string().min(1).max(255),
+        description: z.string().min(1)
+    });
+
+    const validation = await createIsssueSchema.safeParse(body);
+    
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, { status: 400 })
+
+    const newIssue = await prisma.issue.create({
+        data: {
+            title: body.title,
+            description: body.description
+        }
+    })
+
+    return NextResponse.json(newIssue, { status: 201 });
+
+}
