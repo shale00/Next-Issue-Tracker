1cea43c0c7c229b372fcddc1d4b22dc9f727ecef
sth <sth0067@gmail.com>
2023-10-30 15:43:14 -0500

    added error handling to new issue page handleSubmit function

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -6,15 +6,15 @@ import prisma from '@/prisma/client';
 export async function POST(request: NextRequest) {
     const body = await request.json();
 
-    const createIsssueSchema = await z.object({
+    const createIsssueSchema = z.object({
         title: z.string().min(1).max(255),
         description: z.string().min(1)
     });
 
-    const validation = await createIsssueSchema.safeParse(body);
+    const validation = createIsssueSchema.safeParse(body);
     
     if (!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400 })
+        return NextResponse.json(validation.error.format(), { status: 400 })
 
     const newIssue = await prisma.issue.create({
         data: {
