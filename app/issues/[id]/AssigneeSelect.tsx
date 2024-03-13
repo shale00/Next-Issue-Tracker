256f128b7818e27c1ee5c597acc6cf60a0f83e88
sth <sth0067@gmail.com>
2024-03-13 18:56:29 -0500

    refactored the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -8,20 +8,21 @@ import Skeleton from "react-loading-skeleton";
 import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000,
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
+
+  const assignIssue = (userId: string) => {
+    axios
+      .patch("/xapi/issues/" + issue.id, {
+        assignedToUserId: userId || null,
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved.");
+      });
+  };
 
   // reactQuery replaces the use of state and effect hooks
   // const [users, setUsers] = useState<User[]>([]);
@@ -37,15 +38,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   return (
     <>
       <Select.Root
-        onValueChange={(userId) => {
-          axios
-            .patch("/xapi/issues/" + issue.id, {
-              assignedToUserId: userId || null,
-            })
-            .catch(() => {
-              toast.error("Changes could not be saved.");
-            });
-        }}
+        onValueChange={assignIssue}
         defaultValue={issue.assignedToUserId || ""}
       >
         <Select.Trigger placeholder="Assign..." />
@@ -65,5 +58,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, //60s can use a longer time*
+    retry: 3,
+  });
 
 export default AssigneeSelect;
