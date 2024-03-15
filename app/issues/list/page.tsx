f9949d8224719e11615d9d2d234d76d70a7bee71
sth <sth0067@gmail.com>
2024-03-15 18:48:07 -0500

    added sorting issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -12,22 +12,28 @@ interface Props {
 
 const IssuesPage = async ({ searchParams }: Props) => {
   const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    {
-      label: "Issue",
-      value: "title",
-    },
+    { label: "Issue", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
 
   const statuses = Object.values(Status);
+
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (
